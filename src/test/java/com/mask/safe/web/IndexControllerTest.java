bc521a5962ab33ac973d4936be24115abdda44ee
kimjaeyun <je941004@naver.com>
2020-03-07 16:54:32 +0900

    h2 console 설정 및 IndexController 수정 (RestController-> Controller)

--- src/test/java/com/mask/safe/web/IndexControllerTest.java
+++ src/test/java/com/mask/safe/web/IndexControllerTest.java
@@ -1,8 +1,29 @@
 package com.mask.safe.web;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IndexControllerTest {
+
+  @Autowired
+  private TestRestTemplate testRestTemplate;
+
+  @Test
+  public void 메인페이지_로딩() {
+    //when
+    String body = this.testRestTemplate.getForObject("/",String.class);
+
+    //then
+    assertThat(body).contains("마스크");
+  }
 
 }
