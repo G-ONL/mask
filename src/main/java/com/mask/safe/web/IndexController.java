ffe7810c5919dcf472deab70d159ee877bf240dc
kimjaeyun <je941004@naver.com>
2020-03-13 20:50:09 +0900

    test 삭제

--- src/main/java/com/mask/safe/web/IndexController.java
+++ src/main/java/com/mask/safe/web/IndexController.java
@@ -1,16 +1,17 @@
 package com.mask.safe.web;
 
 import com.mask.safe.service.MasksService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-  @Autowired
-  MasksService masksService;
+  private final MasksService masksService;
 
   @GetMapping("/")
   public String index(Model model) {
