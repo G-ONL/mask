11baca47a4c3bb79faf40c7ec0fdf12dc3a66cfd
kimjaeyun <je941004@naver.com>
2020-03-30 23:37:39 +0900

    페이지 처리 (수정 필요)

--- src/main/java/com/mask/safe/web/IndexController.java
+++ src/main/java/com/mask/safe/web/IndexController.java
@@ -1,25 +1,15 @@
 package com.mask.safe.web;
 
-import com.mask.safe.domain.masks.Masks;
-import com.mask.safe.service.MasksService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-  private final MasksService masksService;
-
   @GetMapping("/")
-  public String index(Model model, Pageable pageable) {
-    Page<Masks> masksPage = masksService.findAll(pageable);
-    model.addAttribute("masks", masksPage);
+  public String index() {
     return "index";
   }
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,15 +1,23 @@
+var name = $('#name').val();
+var company = $('#company').val();
+var grade = $('#grade').val();
+var code = $('#code').val();
+
 var index = {
+
   init: function () {
     var _this = this;
     $('#btn-search').on('click', function () {
       _this.search();
     });
+    this.search();
   },
   search: function () {
-    var name = $('#name').val();
-    var company = $('#company').val();
-    var grade = $('#grade').val();
-    var code = $('#code').val();
+    name = $('#name').val();
+    company = $('#company').val();
+    grade = $('#grade').val();
+    code = $('#code').val();
+    page = 0;
 
     console.log("name : " + name + " company : " + company + " grade : " + grade
         + " code : " + code);
@@ -20,7 +28,9 @@ var index = {
         code: code,
         name: name,
         company: company,
-        grade: grade
+        grade: grade,
+        page: page,
+        size: 100
       },
       dataType: 'json',
       contentType: 'application/json; charset=utf-8'
@@ -28,18 +38,32 @@ var index = {
       console.log(data);
       $("#tbody").empty();
       var content = data.content;
-      $("#totalElements").text("검색된 마스크 개수 : "+data.totalElements+" 건");
+      var totalElements = data.totalElements;
+      $("#totalElements").text("검색된 마스크 개수 : " + totalElements + " 건");
 
       for (var i = 0; i < content.length; i++) {
-        name = content[i].name;
-        company = content[i].company;
-        grade = content[i].grade;
-        code = content[i].code;
+        var name = content[i].name;
+        var company = content[i].company;
+        var grade = content[i].grade;
+        var code = content[i].code;
         console.log(name + " --- " + company + " --- " + code);
         var table_body_element = "<tr><td>" + code + "</td><td>" + name
             + "</td><td>" + company + "</td><td>" + grade + "</td></tr>"
         $("#tbody").append(table_body_element);
       }
+
+      $("#nav").remove();
+      $(document).ready(function () {
+        $("table").after('<div id="nav"></div>');
+        var rowPerPage = 100;
+        var rowsTotal = totalElements;
+        var pagesTotal = Math.ceil(rowsTotal / rowPerPage);
+        for (i = 0; i < pagesTotal; i++) {
+          $("<button type=\"button\"></button>").attr("id", i).text(
+              i + 1).appendTo("#nav");
+        }
+        $("#nav button:first").addClass("active");
+      });
 
     }).fail(function (error) {
       console.log("error");
@@ -48,3 +72,48 @@ var index = {
   }
 }
 index.init();
+
+$(document).on('click', "#nav button", function () {
+  console.log("click");
+  $("#nav button").removeClass("active");
+  $(this).addClass("active");
+  var currPage = $(this).attr("id");
+
+  console.log("name : " + name + " company : " + company + " grade : " + grade
+      + " code : " + code);
+
+  $.ajax({
+    type: 'GET',
+    url: '/search',
+    data: {
+      code: code,
+      name: name,
+      company: company,
+      grade: grade,
+      page: currPage,
+      size: 100
+    },
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8'
+  }).done(function (data) {
+    console.log(data);
+    $("#tbody").empty();
+    var content = data.content;
+    var totalElements = data.totalElements;
+    $("#totalElements").text("검색된 마스크 개수 : " + totalElements + " 건");
+
+    for (var i = 0; i < content.length; i++) {
+      var name = content[i].name;
+      var company = content[i].company;
+      var grade = content[i].grade;
+      var code = content[i].code;
+      console.log(name + " --- " + company + " --- " + code);
+      var table_body_element = "<tr><td>" + code + "</td><td>" + name
+          + "</td><td>" + company + "</td><td>" + grade + "</td></tr>"
+      $("#tbody").append(table_body_element);
+    }
+  }).fail(function (error) {
+    console.log("error");
+    alert(error);
+  });
+});
